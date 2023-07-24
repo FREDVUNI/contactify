8da0859b663938f45fd15fc27f52879f5c12d855
TAATA EARL <fredvuni809@gmail.com>
2023-07-24 12:24:46 +0300

    add all necessary components to Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import Hero from '../components/Hero'
+import AddContact from '../components/AddContact'
+import Contacts from '../components/Contacts'
 
 const Home = () => {
   return (
     <>
         <Header/>
+        <Hero/>
+        <AddContact/>
+        <Contacts/>
         <Footer/>
     </>
   )
