0861885cf7a75263453b9185d837569c8426d482
Fred Vuni <fredvuni809@gmail.com>
2023-08-06 12:15:52 +0300

    add a get contacts reducer action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
 export const contactActions = {
   ADD_CONTACT: "add-contact",
+  GET_CONTACTS: "get-contacts",
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,6 +4,8 @@ export const contactReducer = (state, action) => {
   switch (action.type) {
     case contactActions.ADD_CONTACT:
       return [...state, action.payload];
+    case contactActions.GET_CONTACTS:
+      return [action.payload];
     default:
       return state;
   }
