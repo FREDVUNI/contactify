7d046cbf6103c769ef9dd711bbca489f3d18fa73
Fred Vuni <fredvuni809@gmail.com>
2023-08-25 09:13:31 +0300

    add exception for when there's no contacts

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -7,21 +7,25 @@ const Contacts = () => {
   const { state } = useContext(ContactsContext);
   return (
     <div className="add-contact">
-      <table className="contacts-table">
-        <thead>
-          <tr>
-            <th className="fixed-column">Image</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Phone</th>
-          </tr>
-        </thead>
-        <tbody>
-          {state.map((contact, index) => (
-            <Contact contact={contact} key={index} />
-          ))}
-        </tbody>
-      </table>
+      {state.length > 0 ? (
+        <table className="contacts-table">
+          <thead>
+            <tr>
+              <th className="fixed-column">Image</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+            </tr>
+          </thead>
+          <tbody>
+            {state.map((contact, index) => (
+              <Contact contact={contact} key={index} />
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <span className="text-danger">There are no contacts yet.</span>
+      )}
     </div>
   );
 };
